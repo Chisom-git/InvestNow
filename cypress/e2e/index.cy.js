3bef11f094bf9f7c44039d9c987c1dcbe201176a
Chisom-git <chisom94@gmail.com>
2026-06-24 13:01:33 +0100

    InvestNow

---
+++ cypress/e2e/index.cy.js
@@ -0,0 +1,9 @@
+describe('User Login', () => {
+  it('User is able to Login successfully', () => {
+    cy.visit('/')
+    cy.get('#_r_3_-form-item').should('be.visible').click()
+    cy.get('input#_r_3_-form-item').should('exist').type('chisom94@gmail.com')
+    cy.get('input#_r_4_-form-item').should('exist').type('Lagos#234')
+    cy.get('button[type=submit]').contains('Login').should('be.visible').and('exist').click()
+  })
+})
