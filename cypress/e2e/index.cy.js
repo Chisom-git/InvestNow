2a0bee5dde142acc9be464072ee13438e9a7303c
Chisom-git <chisom94@gmail.com>
2026-07-22 02:34:39 +0100

    Updated code

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -5,5 +5,6 @@ describe('User Login', () => {
     cy.get('input#_r_3_-form-item').should('exist').type('chisom94@gmail.com')
     cy.get('input#_r_4_-form-item').should('exist').type('Lagos#234')
     cy.get('button[type=submit]').contains('Login').should('be.visible').and('exist').click()
+    // cy.get('button').contains('Login').click()
   })
 })
